e6861d44ec160325a2abd0f08d427938f4834789
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-01-22 18:14:41 +0530

    My First Custom Component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,17 @@
-import { StyleSheet, View, Text, Image } from "react-native";
+import { View } from "react-native";
 import React from "react";
-import products from "../../../assets/data/products"
-import { Colors } from "../../constants/theme";
+import products from "../../../assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
 
 const product = products[0];
 
-const index = () => {
+const MenuScreen = () => {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 };
 
-export default index;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: { fontSize: 18, fontWeight: "600",marginVertical:10 },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  }
-});
+export default MenuScreen;
