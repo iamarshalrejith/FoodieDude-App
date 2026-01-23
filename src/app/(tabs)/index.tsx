70ab2ddd4d71cc9c45a9431bfbffedc7f02430d0
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-01-23 18:13:58 +0530

    4.FlatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 import React from "react";
 import products from "../../../assets/data/products";
 import ProductListItem from "@/src/components/ProductListItem";
 
-const product = products[0];
-
 const MenuScreen = () => {
   return (
     <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
+      <FlatList
+        data={products} // requires array of items
+        renderItem={({ item }) => <ProductListItem product={item} />} // should be a function tells flatlist how one single item from data be rendered
+        numColumns={2} // i want in 2 cols
+        contentContainerStyle={{gap:10,padding:10}} // fr row 
+        columnWrapperStyle={{gap:10}} // fr col
+
+      />
     </View>
   );
 };
