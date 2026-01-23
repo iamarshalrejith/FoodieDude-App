8314efd13f35df308d38ae8192f2726008d3db0d
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-01-23 17:39:39 +0530

    3.Type Safe Components

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,13 +2,20 @@ import { StyleSheet, View, Text, Image } from "react-native";
 import React from "react";
 import products from "../../assets/data/products"
 import { Colors } from "../constants/theme";
+import { Product } from "../types"
 
 // type Product = (typeof products)[number] // Gives me the union of all possible element types in this array.
 
-const ProductListItem = ({product}) => {
+const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png'
+
+type ProductListItemProps = {
+    product : Product
+}
+
+const ProductListItem = ({product}:ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image}/>
+      <Image source={{uri: product.image || defaultPizzaImage }} style={styles.image}/>
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
